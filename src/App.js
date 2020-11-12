c99d5be34e213e63b77a981c99ce881d0169af75
Sam Rosenthal <samrrosenthal@gmail.com>
2020-11-11 21:15:34 -0800

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import TopNav from './components/topnav/navbar';
-
+import React from "react";
+import TopNav from "./components/topnav/navbar";
+import TabSets from "./components/tabsets/tabsets";
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <TabSets />
     </div>
   );
 }
